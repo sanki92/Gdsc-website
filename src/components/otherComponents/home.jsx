4e73ef0cc90831e04d0a4f32de1178476b79f08e
Shankyy <sankalpt92@gmail.com>
2023-09-23 09:23:56 +0530

    added react Icon package

--- src/components/otherComponents/home.jsx
+++ src/components/otherComponents/home.jsx
@@ -12,6 +12,9 @@ import linkedin_logo from '../../assets/contactUs/LinkedIn.png';
 import instagram_log from '../../assets/contactUs/instagram.jpg';
 import discord_logo from '../../assets/contactUs/discord.png';
 import Side_logo from '../../assets/side.png';
+import {FaInstagramSquare} from 'react-icons/fa'
+import {BsLinkedin} from 'react-icons/bs'
+import {IoLogoDiscord} from 'react-icons/io5'
 
 
 
@@ -22,10 +25,10 @@ const Home = () => {
             <div style={{ backgroundImage: `url(${Side_logo})`}}>
 
             </div>
-        <div name='home' className='w-full h-auto ' style={{ backgroundImage: `url(${backgroundImage})`}}  >
+        <div id='home' className='w-full h-auto ' style={{ backgroundImage: `url(${backgroundImage})`}}  >
             <Header/>
             
-            <div name='aboutus' className='max-w-full md:max-w-[1000px] mx-auto px-[20px] md:px-[80px] flex flex-col pt-10 md:pt-20 h-full gap-40 md:gap-64'>
+            <div id='aboutus' className='max-w-full md:max-w-[1000px] mx-auto px-[20px] md:px-[80px] flex flex-col pt-10 md:pt-20 h-full gap-40 md:gap-64'>
 
                 <div className='flex flex-col pt-9 gap-4'>
                     <div className='flex flex-row gap-2'>
@@ -57,7 +60,7 @@ const Home = () => {
                 </div>
 
 
-                <div name='ourteam' className='flex flex-col pt-9 gap-4 '>
+                <div id='ourteam' className='flex flex-col pt-9 gap-4 '>
                         <img src={team_logo} width={100} height={60} alt="logo" className='mr-5' />
                     <div className='flex flex-row gap-2 items-center'>
                         <p className=' font-medium text-red-700 text-5xl'> Our Team</p>
@@ -76,7 +79,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div name='events' className='flex flex-col pt-9 gap-4'>
+                <div id='events' className='flex flex-col pt-9 gap-4'>
                         <img src={calander_logo} width={100} height={60} alt="logo" />
                     <div className='flex flex-row gap-2 items-center'>
                         <p className='font-medium text-5xl text-[#DEB200]'> Our Events</p>
@@ -94,26 +97,26 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div name="contact" className='flex flex-col gap-4 pt-10'>
+                <div id="contact" className='flex flex-col gap-4 pt-10'>
                     <div className='flex flex-row gap-2 items-center'>
                         <img src={contact_logo} width={80} height={30} alt="logo" />
                         <p className='font-medium text-5xl text-[#00681C]'>Contact Us</p>
                     </div>
                     <div>
-                        <p className='text-[#7B7B7B] text-2xl  mt-5'>Feel free to connect with us/Don’t be hesitant to reach out to us!/Our social media game? Stronger than your Wi-Fi connection./We're social butterflies on the internet – follow our digital footprints!</p>
+                        <p className='text-[#7B7B7B] text-2xl  mt-5'>We're social butterflies on the internet – follow our digital footprints!</p>
                     </div>
 
-                    <div className='flex flex-row gap-8 mt-4'>
+                    <div className='flex flex-row gap-8 mt-4 '>
                         <a href="https://in.linkedin.com/company/gdsc-mitadt" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedin_logo} className="h-15" alt="LinkedIn" />
+                            <BsLinkedin className='text-5xl text-[#265CAC]'/>
                         </a>
 
                         <a href="https://www.instagram.com/gdsc.mitadt/" target="_blank" rel="noopener noreferrer">
-                            <img src={instagram_log} className="h-10" alt="instagram_logo" />
+                            <FaInstagramSquare className='text-5xl text-[#265CAC]'/>
                         </a>
                         
                         <a href="https://discord.com/invite/Vnwn2RAQzw" target="_blank" rel="noopener noreferrer">
-                            <img src={discord_logo} className="h-15" alt="discord_logo" />
+                            <IoLogoDiscord className='text-5xl text-[#265CAC]'/>
                         </a>
                     </div>
 
